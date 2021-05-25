7bb851c2798d5dbd712c8e570a6d94310f8a110d
simonsaysx <82106733+simonsaysx@users.noreply.github.com>
2021-05-25 15:35:29 -0400

    Added remove car function, polished view garage, and finalized program functions

--- Dream Garage.py
+++ Dream Garage.py
@@ -10,19 +10,30 @@ garage = {}
 
 def dream_garage():
     greeting()
-    if len(garage) == 0:
-        answer = input("Would you like to add your first car? Y/N")
+    answer = ""
+    while answer != "N":
+        answer = input("Would you like to add a vehicle? Y/N \n")
         if answer == "Y":
             add_car()
+    
+    goodbye()
+    
 
 def greeting():
     print("Welcome to Dream Garage Builder. This program will help you track your Dream 3 car garage.")
 
+def goodbye():
+    print("Thank you for using my Dream Garage Builder.")
+
+def view_garage(garage):
+    for car, values in garage.items():
+        print(car, ":", values)
+
 def add_car():
-    if len(garage) <= 3:
-        model_name = input("What is the model name you would like to add?")
+    if len(garage) < 3:
+        model_name = input("What is the model name you would like to add? \n")
         new_car = Car(model_name)
-        additional_info = input("What other info would you like to add? Brand, Year, Price, Category.")
+        additional_info = input("What other info would you like to add? Brand, Year, Price, Category. \n")
         additional_info = additional_info.lower()
         if "brand" in additional_info:
             new_car.brand = input("Please enter brand name: ")
@@ -31,11 +42,29 @@ def add_car():
         if "price" in additional_info:
             new_car.price = input("Please enter vehicle price: $")
         if "category" in additional_info:
-            new_car.category = input("Please enter vehicle category: (Ex. Sport, Luxury, SUV)")
-        cars[new_car.model] = [new_car.brand, new_car.year, new_car.price, new_car.category]
+            new_car.category = input("Please enter vehicle category: (Ex. Sport, Luxury, SUV) \n")
+        garage[new_car.model] = [new_car.brand, new_car.year, new_car.price, new_car.category]
         print("Successfully added {new_car} to garage.".format(new_car = new_car.model))
     else:
         print("Please remove an exsisting vehicle to add another vehicle.")
+        ans = input("Would you like to remove a vehicle? Y/N \n")
+        if ans == "Y":
+            vehicle = input("Please enter the vehicle model or 'garage' to view current vehicles. \n")
+            if vehicle == 'garage':
+                view_garage(garage)
+            else:
+                if vehicle in garage.keys():
+                    remove_car(vehicle)
+                else:
+                    print("Could not find specified vehicle in garage.")
+    view_g = input("Would you like to view your garage? Y/N \n")
+    if view_g =="Y":
+        view_garage(garage)
 
+                    
+
+def remove_car(model):
+    garage.pop(model)
+    print("{} successfully removed from garage.".format(model))
 
 dream_garage()
