81fa9acc8b6f36bb9f7ad2782314bfa24b441d44
simonsaysx <82106733+simonsaysx@users.noreply.github.com>
2021-05-25 14:42:22 -0400

    Added greeting and Car Class

---
+++ Dream Garage.py
@@ -0,0 +1,18 @@
+class Car:
+    def __init__(self, model, brand, year = None, price = None, category = None):
+        self.model = model
+        self.brand = brand
+        self.year = year
+        self.price = price
+        self.category = category
+
+cars = {}
+
+def dream_garage():
+    greeting()
+    
+
+def greeting():
+    print("Welcome to Dream Garage Builder. This program will help you track your Dream 3 car garage.")
+
+dream_garage()
